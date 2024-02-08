fd0cb673438e68496b772b50b9a5965507b89f82
bochra-mayara <bochra-mayara>
2024-02-08 17:06:41 +0100

    depend3

--- src/main/java/com/mayara/demo/ApplicationConfig.java
+++ src/main/java/com/mayara/demo/ApplicationConfig.java
@@ -3,20 +3,26 @@ package com.mayara.demo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
-    @Qualifier("bean1")
     public MyFirstClass myFirstClass(){
         return new MyFirstClass("First Bean");
 
     }
 
-    @Bean
-    @Qualifier("bean2")
+    @Bean("bean2")
     public MyFirstClass myFirstClass2(){
         return new MyFirstClass("Second Bean");
+
+    }
+
+    @Bean
+    //@Primary
+    public MyFirstClass myFirstClass3(){
+        return new MyFirstClass("Third Bean");
 
     }
 
--- src/main/java/com/mayara/demo/MyFirstService.java
+++ src/main/java/com/mayara/demo/MyFirstService.java
@@ -1,14 +1,15 @@
 package com.mayara.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
+    @Autowired
+    @Qualifier("bean2")
     private MyFirstClass myFirstClass;
-    public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
-        this.myFirstClass = myFirstClass;
-    }
+
 
 
 
