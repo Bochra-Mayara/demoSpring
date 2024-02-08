8fcd81573c44debde2b9f8bb6f54cd8d0489bb14
bochra-mayara <bochra-mayara>
2024-02-08 23:00:36 +0100

    depend

--- src/main/java/com/mayara/demo/DemoApplication.java
+++ src/main/java/com/mayara/demo/DemoApplication.java
@@ -11,7 +11,9 @@ public class DemoApplication {
 		var ctx = SpringApplication.run(DemoApplication.class, args);
 		MyFirstService myFirstService= ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellStory());
-
+		System.out.println(myFirstService.getJavaVersion());
+		System.out.println(myFirstService.getOSName());
+		System.out.println(myFirstService.readProperty());
 
 	}
 
--- src/main/java/com/mayara/demo/MyFirstService.java
+++ src/main/java/com/mayara/demo/MyFirstService.java
@@ -2,18 +2,39 @@ package com.mayara.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
-    @Autowired
-    @Qualifier("bean2")
+
     private MyFirstClass myFirstClass;
 
 
 
+    private Environment environment;
+    @Autowired
+    public void setMyFirstClass(@Qualifier("bean2") MyFirstClass myFirstClass
+    ){
+        this.myFirstClass = myFirstClass;
+    }
+
 
     public String tellStory(){
         return "the dependency s saying " + myFirstClass.sayHello();
+    }
+    public String getJavaVersion(){
+        return environment.getProperty("java.version");
+    }
+
+    public String getOSName(){
+        return environment.getProperty("os.name");
+    }
+    public String readProperty(){
+        return environment.getProperty("my.custom.property");
+    }
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
